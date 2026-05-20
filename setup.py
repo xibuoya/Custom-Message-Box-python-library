b0f89d6fb90158229b80fede4e1876414d7b50a7
PyApps <xibuoya@gmail.com>
2026-05-20 13:11:56 +0100

    Create setup.py
    Signed-off-by: PyApps <xibuoya@gmail.com>

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Custom_Message_Box",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "customtkinter",
+    ],
+)
